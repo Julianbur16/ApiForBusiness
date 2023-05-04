820595f5d580bd50717a9acc7c0fb78763e40444
Alejandro Burbano <Julianbur16@gmail.com>
2023-05-03 23:38:44 -0500

    webhook

--- app/Http/Controllers/WhatsappController.php
+++ app/Http/Controllers/WhatsappController.php
@@ -97,8 +97,6 @@ class WhatsappController extends Controller
                         ));
                         $result = curl_exec($ch);
                         curl_close($ch);
-                        $resultdeco = json_decode($result);
-                        $text1 = $resultdeco->choices[0]->text;
 
 
                         
@@ -107,7 +105,7 @@ class WhatsappController extends Controller
                     'json' => [
                         'messaging_product' => 'whatsapp',
                         'to' => $from,
-                        'text' => ['body' => 'Ack: ' . $text1],
+                        'text' => ['body' => 'Ack: ' . $result],
                     ],
                 ]);
             }
