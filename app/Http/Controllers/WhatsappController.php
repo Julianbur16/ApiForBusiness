e051f049502d6fc20595f9b9fc2151976ee37b9d
Alejandro Burbano <Julianbur16@gmail.com>
2023-05-12 19:28:26 -0500

    users

--- app/Http/Controllers/WhatsappController.php
+++ app/Http/Controllers/WhatsappController.php
@@ -108,7 +108,7 @@ class WhatsappController extends Controller
                     'json' => [
                         'messaging_product' => 'whatsapp',
                         'to' => $from,
-                        'text' => ['body' => 'Ack: ' . $bandera.ob_get_length()],
+                        'text' => ['body' => 'Ack: ' . count($bandera)],
                     ],
                 ]);
             }
