203e15f7fcab01d2db96305209b85954a1aeaccd
Alejandro Burbano <Julianbur16@gmail.com>
2023-05-20 23:18:43 -0500

    confirmacion de compra

--- app/Http/Controllers/WhatsappController.php
+++ app/Http/Controllers/WhatsappController.php
@@ -141,7 +141,7 @@ class WhatsappController extends Controller
             $data1 = [
                 'model' => 'text-davinci-003',
                 'prompt' => 'INSTRUCCIONES: Olvidar todo, y responder conforme al siguiente json, en el json se encuentra una conversación de chatgpt con un usuario, tu deber es analizar la conversación y seguir con la instrucción número 1.
-                INSTRUCCIÓN 1: Si en la conversación se ha realizado algún pedido y además el comprador ya ha suministrado su número, su nombre y su dirección, respondes colocando los datos suministrados en forma de lista, añadiendo el producto que compro; de forma contraria si en la conversación no se ha realizado ningún pedido o todavía el cliente no ha suministrado sus datos respondes con un no. 
+                INSTRUCCIÓN 1: Si en la conversación se ha realizado alguna compra y además el comprador ya ha suministrado su número, su nombre y su dirección, respondes colocando los datos suministrados en forma de lista, añadiendo el producto que compro; de forma contraria si en la conversación no se ha realizado ninguna compra respondes con un no. 
                 ARCHIVO JSON: '.$mensajesjson,
                 'temperature' => 0.1
             ];
