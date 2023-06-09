927fe4a2825b2490751a43866360a10afb8bdaf6
Alejandro Burbano <Julianbur16@gmail.com>
2023-06-09 15:51:05 -0500

    ajustes

--- app/Http/Controllers/WhatsappController.php
+++ app/Http/Controllers/WhatsappController.php
@@ -325,13 +325,13 @@ class WhatsappController extends Controller
                     return response('Success', 200);
                 }
 
-                if($body['entry'][0]['changes'][0]['value']['messages'][0]['image']['id']){
+                if(isset($body['entry'][0]['changes'][0]['value']['messages'][0]['image']['id'])){
                     $id_image=$body['entry'][0]['changes'][0]['value']['messages'][0]['image']['id'];
                     $this->enviarmsm("121497920919503", "573157683957", 'imagen id '.$id_image); //envia mensaje de whatsapp   
                     return response('Success', 200);
                 }
 
-                if($body['entry'][0]['changes'][0]['value']['messages'][0]['audio']['id']){
+                if(isset($body['entry'][0]['changes'][0]['value']['messages'][0]['audio']['id'])){
                     $id_image=$body['entry'][0]['changes'][0]['value']['messages'][0]['audio']['id'];
                     $this->enviarmsm("121497920919503", "573157683957", 'audio id '.$id_image); //envia mensaje de whatsapp   
                     return response('Success', 200);
