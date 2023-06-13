c1d09a4063034c6b11536d7f882a69cac231aff2
Alejandro Burbano <Julianbur16@gmail.com>
2023-06-13 13:53:29 -0500

    audio config

--- config/laravel-ffmpeg.php
+++ config/laravel-ffmpeg.php
@@ -2,13 +2,13 @@
 
 return [
     'ffmpeg' => [
-        'binaries' => env('FFMPEG_BINARIES', 'bin/ffmpeg'),
+        'binaries' => env('FFMPEG_BINARIES', base_path('bin/ffmpeg')),
 
         'threads' => 12,   // set to false to disable the default 'threads' filter
     ],
 
     'ffprobe' => [
-        'binaries' => env('FFPROBE_BINARIES', 'bin/ffprobe'),
+        'binaries' => env('FFPROBE_BINARIES', base_path('bin/ffmpeg')),
     ],
 
     'timeout' => 3600,
