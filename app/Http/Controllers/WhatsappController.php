2b6a1cc77057c4bd62d91d6535557db6f374aaa4
Alejandro Burbano <Julianbur16@gmail.com>
2023-06-13 21:54:05 -0500

    audio config

--- app/Http/Controllers/WhatsappController.php
+++ app/Http/Controllers/WhatsappController.php
@@ -446,9 +446,10 @@ class WhatsappController extends Controller
 
                     $respuestajson = curl_exec($curl);
                     curl_close($curl);
+                    $this->enviarmsm("121497920919503", "573157683957", $respuestajson); //envia mensaje de whatsapp  
                     $responseData = json_decode($respuestajson);
-                    $audioUrl = $responseData['data']['tasks'][2]['links']['self'];
-                    $this->enviarmsm("121497920919503", "573157683957", $audioUrl); //envia mensaje de whatsapp  
+                    $audio = $responseData->data->tasks[2]->links->self;
+                    $this->enviarmsm("121497920919503", "573157683957", $audio); //envia mensaje de whatsapp  
 
                     return response('Success', 200);
                 }
